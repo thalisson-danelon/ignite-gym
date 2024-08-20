e1616f2a65d1d0518207252192a50d2cdaf733f9
thalisson-danelon <thalisson.github@gmail.com>
2024-08-19 21:52:38 -0300

    feat: SignIn screen created

--- App.tsx
+++ App.tsx
@@ -1,23 +1,18 @@
 import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 import { Loading } from '@components/Loading';
 
 import { config } from './config/gluestack-ui.config';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="trasparent" translucent />
-      {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text>Home</Text>
-        </Center>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   )
     ;
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,53 @@
+import { Center, Heading, Image, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+import BackgroundImg from '@assets/background.png';
+import Logo from '@assets/logo.svg';
+
+export function SignIn() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          position="absolute"
+        />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
+          <Center gap="$2">
+            <Heading color="$gray100">
+              Acesse sua conta
+            </Heading>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Senha" secureTextEntry />
+            <Button title="Acessar" />
+          </Center>
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+              Ainda não tem acesso?
+            </Text>
+            <Button title="Criar conta" variant="outline" />
+          </Center>
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+}
