65d796d9663a6a0cd32bcfc06fc898ab6dff0a8c
thalisson-danelon <thalisson.github@gmail.com>
2024-08-20 14:19:44 -0300

    feat: routes created

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 
+import { Routes } from '@routes/index';
+
 import { Loading } from '@components/Loading';
 
 import { config } from './config/gluestack-ui.config';
-import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="trasparent" translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
     ;
