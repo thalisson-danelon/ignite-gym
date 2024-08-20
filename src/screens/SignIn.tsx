408341560f17f621648dd8ed5b709210578634b5
thalisson-danelon <thalisson.github@gmail.com>
2024-08-19 22:07:29 -0300

    feat: SignIn screen updated

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,53 +1,59 @@
-import { Center, Heading, Image, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import { Center, Heading, Image, KeyboardAvoidingView, ScrollView, Text, VStack } from '@gluestack-ui/themed';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 import BackgroundImg from '@assets/background.png';
 import Logo from '@assets/logo.svg';
+import { Platform } from 'react-native';
 
 export function SignIn() {
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <VStack flex={1} bg="$gray700">
-        <Image
-          w="$full"
-          h={624}
-          source={BackgroundImg}
-          defaultSource={BackgroundImg}
-          alt="Pessoas treinando"
-          position="absolute"
-        />
-        <VStack flex={1} px="$10" pb="$16">
-          <Center my="$24">
-            <Logo />
-            <Text color="$gray100" fontSize="$sm">
-              Treine sua mente e o seu corpo
-            </Text>
-          </Center>
-          <Center gap="$2">
-            <Heading color="$gray100">
-              Acesse sua conta
-            </Heading>
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <Input placeholder="Senha" secureTextEntry />
-            <Button title="Acessar" />
-          </Center>
-          <Center flex={1} justifyContent="flex-end" mt="$4">
-            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-              Ainda não tem acesso?
-            </Text>
-            <Button title="Criar conta" variant="outline" />
-          </Center>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <VStack flex={1} bg="$gray700">
+          <Image
+            w="$full"
+            h={624}
+            source={BackgroundImg}
+            defaultSource={BackgroundImg}
+            alt="Pessoas treinando"
+            position="absolute"
+          />
+          <VStack flex={1} px="$10" pb="$16">
+            <Center my="$24">
+              <Logo />
+              <Text color="$gray100" fontSize="$sm">
+                Treine sua mente e o seu corpo
+              </Text>
+            </Center>
+            <Center gap="$2">
+              <Heading color="$gray100">
+                Acesse sua conta
+              </Heading>
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+              <Input placeholder="Senha" secureTextEntry />
+              <Button title="Acessar" />
+            </Center>
+            <Center flex={1} justifyContent="flex-end" mt="$4">
+              <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+                Ainda não tem acesso?
+              </Text>
+              <Button title="Criar conta" variant="outline" />
+            </Center>
+          </VStack>
         </VStack>
-      </VStack>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
