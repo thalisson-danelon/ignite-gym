d22300619e2e39e864d63022fa7d9baf62075e26
thalisson-danelon <thalisson.github@gmail.com>
2024-08-21 23:44:20 -0300

    feat: Group component created

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
 import { VStack } from "@gluestack-ui/themed";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <Group name="Costas" isActive={false} />
     </VStack>
   );
 }
