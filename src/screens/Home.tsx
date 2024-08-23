835347877b0e2f6fffde202634d06baaba78f7d8
thalisson-danelon <thalisson.github@gmail.com>
2024-08-23 15:20:32 -0300

    feat: Home screen updated

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,58 @@
-import { VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
+  const [exercises, setExercises] = useState([
+    'Puxada Frontal',
+    'Remada unilateral',
+    'Remada curvada',
+    'Levantamento terra',
+    'Rosca direta',
+    'Rosca martelo',
+  ])
+  const [groups, setGroups] = useState(['Costas', 'Ombro', 'Bíceps', 'Tríceps'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" isActive={false} />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>
+          <Group
+            name={item}
+            isActive={item.toLowerCase() === groupSelected.toLowerCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        }
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercises.length}
+          </Text>
+        </HStack>
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 }
