07e4d08468dbed27651daa613aeffba23823a3b1
thalisson-danelon <thalisson.github@gmail.com>
2024-08-23 15:37:14 -0300

    feat: ScreenHeader component created

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,11 @@
-import { Center, Text } from '@gluestack-ui/themed';
+import { Center } from '@gluestack-ui/themed';
+
+import { ScreenHeader } from "@screens/ScreenHeader";
 
 export function History() {
   return (
     <Center flex={1}>
-      <Text>History</Text>
+      <ScreenHeader title="Histórico" />
     </Center>
   );
 }
