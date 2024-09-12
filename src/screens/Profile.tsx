8bc917487f46adee3e9ac185730a868d78a7947d
thalisson-danelon <thalisson.github@gmail.com>
2024-09-12 07:22:49 -0300

    feat: created the handleUserPhotoSelect function

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,38 @@
-import { Platform, TouchableOpacity } from "react-native";
+import { Alert, Platform, TouchableOpacity } from "react-native";
 import { Center, Heading, KeyboardAvoidingView, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
+import { useState } from "react";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState('https://github.com/thalisson-danelon.png')
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+      if (photoSelected.canceled) return;
+      const photoUri = photoSelected.assets[0].uri;
+      if (!photoUri) return;
+      const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as { size: number };
+      if (photoInfo.size / 1024 / 1024 > 5) {
+        return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB');
+      }
+      setUserPhoto(photoUri);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
@@ -17,7 +43,7 @@ export function Profile() {
         <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
           <Center mt="$6" px="$10">
             <UserPhoto
-              source={{ uri: 'https://github.com/thalisson-danelon.png' }}
+              source={{ uri: userPhoto }}
               alt="Imagem do usuário"
               size="xl"
             />
@@ -29,6 +55,7 @@ export function Profile() {
                 fontSize="$md"
                 mt="$2"
                 mb="$8"
+                onPress={handleUserPhotoSelect}
               >
                 Alterar imagem
               </Text>
