7480979f2cc39f9f07cdd05e8a540f8741ba05dd
thalisson-danelon <thalisson.github@gmail.com>
2024-09-13 09:05:31 -0300

    feat: using FormControll at login and in create account

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,9 @@
 import { Platform } from 'react-native';
+import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from '@react-navigation/native';
 import { Center, Heading, Image, KeyboardAvoidingView, ScrollView, Text, VStack } from '@gluestack-ui/themed';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from 'yup';
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
@@ -9,13 +12,33 @@ import Logo from '@assets/logo.svg';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import React from "react";
 
+type FormDataProps = {
+  email: string;
+  password: string;
+}
+
+const EMAIL_REGX = /^[a-zA-Z0-9_\-]+(\.[a-zA-Z0-9_\-]+)*@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/i;
+
+const signInSchema = yup.object({
+  email: yup.string().required('Informe o e-mail').matches(EMAIL_REGX, 'E-mail inválido'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha precisa ter pelo menos 6 dígitos'),
+});
 
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signInSchema)
+  });
+
   function handleNewAccount() {
     navigation.navigate('signUp');
+  }
+
+  function handleSignIn({ email, password }: FormDataProps) {
+    console.log({ email, password });
   }
 
   return (
@@ -47,13 +70,39 @@ export function SignIn() {
               <Heading color="$gray100">
                 Acesse sua conta
               </Heading>
-              <Input
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
+
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="E-mail"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.email?.message}
+                  />
+                )}
               />
-              <Input placeholder="Senha" secureTextEntry />
-              <Button title="Acessar" />
+
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.password?.message}
+                    onSubmitEditing={handleSubmit(handleSignIn)}
+                    returnKeyType="send"
+                  />
+                )}
+              />
+
+              <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
             </Center>
             <Center flex={1} justifyContent="flex-end" mt="$4">
               <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import { Platform } from 'react-native';
+import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from '@react-navigation/native';
 import { Center, Heading, Image, KeyboardAvoidingView, ScrollView, Text, VStack, } from '@gluestack-ui/themed';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from 'yup';
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
-
 import BackgroundImg from '@assets/background.png';
-import Logo from '@assets/logo.svg';
 
+import Logo from '@assets/logo.svg';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
+
+const EMAIL_REGX = /^[a-zA-Z0-9_\-]+(\.[a-zA-Z0-9_\-]+)*@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/i;
+
+const signUpSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').matches(EMAIL_REGX, 'E-mail inválido'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha precisa ter pelo menos 6 dígitos'),
+  password_confirm: yup.string().required('Confirme a senha').oneOf([
+    yup.ref('password'),
+    ""
+  ], "As senhas precisam ser iguais"),
+});
+
 export function SignUp() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
   }
 
   return (
@@ -45,14 +73,65 @@ export function SignUp() {
             </Center>
             <Center gap="$2" flex={1}>
               <Heading color="$gray100">Crie sua conta</Heading>
-              <Input placeholder="Nome" />
-              <Input
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
+
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Nome"
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.name?.message}
+                  />
+                )}
               />
-              <Input placeholder="Senha" secureTextEntry />
-              <Button title="Criar e acessar" />
+
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="E-mail"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.email?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.password?.message}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="password_confirm"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Confirmar Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                    onSubmitEditing={handleSubmit(handleSignUp)}
+                    returnKeyType="send"
+                    errorMessage={errors.password_confirm?.message}
+                  />
+                )}
+              />
+
+              <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
             </Center>
             <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack} />
           </VStack>
